6d29b8692572c0577e7338bb73c31346979dabbd
Jerry Toland <approlific@gmail.com>
2026-04-27 06:08:29 -0500

    Initial Commit

---
+++ BillSplitter/BillSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitterApp.swift
+//  BillSplitter
+//
+//  Created by Jerry Toland on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
